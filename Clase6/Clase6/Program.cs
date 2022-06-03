b244302eaef5ec0ade7d392d00812bc60a20cb54
menendezSofia <42209265+menendezSofia@users.noreply.github.com>
2022-06-02 21:01:59 -0300

    ejercicio clase 5

---
+++ Clase6/Clase6/Program.cs
@@ -0,0 +1,28 @@
+﻿var opcion = "s";
+
+while (opcion.ToLower() == "s")
+{
+    Console.Clear();
+    Console.WriteLine("Ingrese nombre: ");
+    var nombre = Console.ReadLine();
+    Console.WriteLine("¡Hola " + nombre + "!");
+    Console.WriteLine("¿Quiere continuar? S/N");
+    opcion = Console.ReadLine();
+
+    while (opcion != "s" && opcion != "n")
+    {
+        Console.WriteLine("opción no válida, ingrese nuevamente");
+        Console.WriteLine("¿Quiere continuar? S/N");
+        opcion = Console.ReadLine();
+
+        if (opcion.ToLower() == "n")
+        {
+            Console.WriteLine("Programa finalizado...");
+        }
+        else
+        {
+            continue;   
+        }
+    }
+}
+
